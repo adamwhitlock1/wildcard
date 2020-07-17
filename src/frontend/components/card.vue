a77a1a1b11ab18f3f31de5726f2dcba96c17ea4a
Adam Whitlock <awhitlock@dealerinspire.com>
2020-07-17 14:08:42 -0600

    update domain info

--- src/frontend/components/card.vue
+++ src/frontend/components/card.vue
@@ -1,52 +1,65 @@
 <template>
-    <div class="p-6 bg-gray-200 border-b-4 border-gray-400 text-white rounded-lg shadow-lg flex">
-
-      <div class="w-1/2">
-        <img class="rounded shadow-lg" :src="example"/>
-      </div>
-
-      <div class="w-1/2 p-1 flex-no-wrap">
-
-        <h3 class="w-full text-green-600 text-sm tracking-tight font-mono text-center">{{ header }}</h3>
-
-        <div class="mt-3 text-gray-100 bg-green-700 h-30 rounded font-light px-4 py-3 border-green-300 border-b-4 shadow-lg">
-          <p class="w-full mt-1 font-black text-green-200 text-sm">Ex: <a :href="'https://wildcard.fun' + example" target="_blank" class="underline">wildcard.fun{{example}}</a></p>
-          <p class="font-light align-baseline text-xs">{{ exampleDescription }}</p>
-        </div>
-
-      </div>
-
+  <div
+    class="p-6 bg-gray-200 border-b-4 border-gray-400 text-white rounded-lg shadow-lg flex"
+  >
+    <div class="w-1/2">
+      <img class="rounded shadow-lg" :src="example" />
     </div>
+
+    <div class="w-1/2 p-1 flex-no-wrap">
+      <h3
+        class="w-full text-green-600 text-sm tracking-tight font-mono text-center"
+      >
+        {{ header }}
+      </h3>
+
+      <div
+        class="mt-3 text-gray-100 bg-green-700 h-30 rounded font-light px-4 py-3 border-green-300 border-b-4 shadow-lg"
+      >
+        <p class="w-full mt-1 font-black text-green-200 text-sm">
+          Ex:
+          <a
+            :href="'wildcard.codestuff.io' + example"
+            target="_blank"
+            class="underline"
+            >wildcard.codestuff.io{{ example }}</a
+          >
+        </p>
+        <p class="font-light align-baseline text-xs">
+          {{ exampleDescription }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex';
-  export default {
-    name: "compExample",
-    props: {
-      header: {
-        type: String,
-        default: "heading"
-      },
-      description: {
-        type: String,
-        default: "description"
-      },
-      example: {
-        type: String,
-        default: "wildcard.fun/r/200/300"
-      },
-      exampleDescription: {
-        type: String,
-        default: "ex description"
-      }
+import { mapState } from "vuex";
+export default {
+  name: "compExample",
+  props: {
+    header: {
+      type: String,
+      default: "heading"
     },
-    computed: {
-      ...mapState['urlString']
+    description: {
+      type: String,
+      default: "description"
+    },
+    example: {
+      type: String,
+      default: "wildcard.codestuff.io/r/200/300"
+    },
+    exampleDescription: {
+      type: String,
+      default: "ex description"
     }
+  },
+  computed: {
+    ...mapState["urlString"]
   }
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/frontend/pages/home.vue
+++ src/frontend/pages/home.vue
@@ -7,46 +7,43 @@
       <p class="font-light">served over {{ totalUsage }} times</p>
     </div>
     <div class="block md:flex -mx-4">
-
       <div class="w-full md:w-1/2 px-4 mt-4">
         <card
-                header='wildcard.fun/r/{w}/{h}'
-                example="/r/250/250"
-                example-description="Results in a random critter image at 250px wide by 250px high."
+          header="wildcard.codestuff.io/r/{w}/{h}"
+          example="/r/250/250"
+          example-description="Results in a random critter image at 250px wide by 250px high."
         ></card>
       </div>
 
       <div class="w-full md:w-1/2 px-4 mt-4">
         <card
-                header="wildcard.fun/{category}/{w}/{h}"
-                example="/dog/250/250"
-                example-description="Get a categorical image from:  cat, dog, bird, bug, tiger, lion, fish"
+          header="wildcard.codestuff.io/{category}/{w}/{h}"
+          example="/dog/250/250"
+          example-description="Get a categorical image from:  cat, dog, bird, bug, tiger, lion, fish"
         ></card>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import card from "../components/card"
+import card from "../components/card";
 import { mapState } from "vuex";
-  export default {
-    components: {
-      card
-    },
-    data() {
-      return {
-        stringExample: "This is a string."
-      }
-    },
-    computed: mapState({
-      totalImages: 'totalImages',
-      totalUsage: 'totalUsage'
-      })
-  }
+export default {
+  components: {
+    card
+  },
+  data() {
+    return {
+      stringExample: "This is a string."
+    };
+  },
+  computed: mapState({
+    totalImages: "totalImages",
+    totalUsage: "totalUsage"
+  })
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>

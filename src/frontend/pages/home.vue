6af22ae8eaad7019a495868b82430f708c008d30
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2025-03-19 12:32:20 -0600

    tweak layout for categories

--- src/frontend/pages/home.vue
+++ src/frontend/pages/home.vue
@@ -23,44 +23,44 @@
         ></card>
       </div>
     </div>
+    <p class="text-4xl pt-12 font-hairline w-full text-center">Categories</p>
     <div class="w-full px-4 mt-4">
-      <p class="text-4xl pt-12 font-hairline w-full text-center">Categories</p>
       <div class="w-full flex flex-wrap">
-        <div class="w-1/2 px-4 mt-4">
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Bird</p>
           <img src="/bird/250/250" alt="Bird" class="w-full h-auto" />
         </div>
-        <div class="w-1/2 px-4 mt-4">
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Bug</p>
           <img src="/bug/250/250" alt="Bug" class="w-full h-auto" />
         </div>
-      </div>
-      <div class="w-full flex flex-wrap">
-        <div class="w-1/2 px-4 mt-4">
+
+
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Cat</p>
           <img src="/cat/250/250" alt="Cat" class="w-full h-auto" />
         </div>
-        <div class="w-1/2 px-4 mt-4">
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Dog</p>
           <img src="/dog/250/250" alt="Dog" class="w-full h-auto" />
         </div>
-      </div>
-      <div class="w-full flex flex-wrap">
-        <div class="w-1/2 px-4 mt-4">
+
+
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Fish</p>
           <img src="/fish/250/250" alt="Fish" class="w-full h-auto" />
         </div>
-        <div class="w-1/2 px-4 mt-4">
+        <div class="w-1/8 p-4 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Lion</p>
           <img src="/lion/250/250" alt="Lion" class="w-full h-auto" />
         </div>
-      </div>
-      <div class="w-full flex flex-wrap">
-        <div class="w-1/2 px-4 mt-4">
+
+
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Tiger</p>
           <img src="/tiger/250/250" alt="Tiger" class="w-full h-auto" />
         </div>
-        <div class="w-1/2 px-4 mt-4">
+        <div class="w-1/8 p-3 mt-4">
           <p class="text-2xl font-hairline w-full text-center">Potato</p>
           <img src="/potato/250/250" alt="Potato" class="w-full h-auto" />
         </div>
